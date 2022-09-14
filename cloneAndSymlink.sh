ed4aaf3feafa32c27262784e13ff1c47014d5799
Nick <cnick1@vt.edu>
2022-09-14 16:13:34 -0700

    Added line to checkout modelOrderReduction branch

--- cloneAndSymlink.sh
+++ cloneAndSymlink.sh
@@ -7,4 +7,7 @@ ln -s /mnt/e/share/sofaInstallationTools /home/nick
 git clone https://github.com/cnick1/softRoboticsMR.git
 ln -s /mnt/e/share/softRoboticsMR /home/nick
 
+cd /home/nick/softRoboticsMR
+git checkout modelOrderReduction
+
 echo "IN ORDER TO USE THE TRASH BIN ON THE DRIVE, EDIT THE MOUNT OPTIONS TO INCLUDE: uid=1000,gid=1000"
