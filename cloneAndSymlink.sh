75458ec6b83e3b81768262d14b8ec6d82e6a53d9
Nick <cnick1@vt.edu>
2022-09-14 15:47:14 -0700

    Added note about trash can

--- cloneAndSymlink.sh
+++ cloneAndSymlink.sh
@@ -6,3 +6,5 @@ ln -s /mnt/e/share/sofaInstallationTools /home/nick/sofaInstallationTools
 
 git clone https://github.com/cnick1/softRoboticsMR.git
 ln -s /mnt/e/share/softRoboticsMR /home/nick/softRoboticsMR
+
+echo "IN ORDER TO USE THE TRASH BIN ON THE DRIVE, EDIT THE MOUNT OPTIONS TO INCLUDE: uid=1000,gid=1000"
