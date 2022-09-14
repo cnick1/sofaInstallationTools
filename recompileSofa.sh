879e49504f10c09404f010f4a1bcc3dbfaf3bf9d
Nick <cnick1@vt.edu>
2022-09-13 23:33:18 -0700

    Various quality of life improvements and automations

--- recompileSofa.sh
+++ recompileSofa.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-version="v21.12"
+if [ $# -eq 0 ]
+then
+    version="master"
+else
+    if [ $1 == stable ]
+    then
+        version="v21.12"
+    else
+        version=$1
+    fi
+fi
 
 cd ~/sofa/src 
 git pull
@@ -21,10 +31,19 @@ mkdir /home/nick/sofa/$version
 
 cd ~/sofa/$version
 
-cmake -G "CodeBlocks - Ninja" -DCMAKE_C_COMPILER=/usr/bin/clang-12 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-12 -DCMAKE_PREFIX_PATH=/home/nick/Qt/5.13.2/gcc_64 -DSOFA_BUILD_METIS=ON -DSOFA_EXTERNAL_DIRECTORIES=/home/nick/sofa-plugins -DSOFTROBOTS_IGNORE_ERRORS=ON -Dpybind11_DIR=/home/nick/.local/lib/python3.8/site-packages/pybind11/share/cmake/pybind11 -DPLUGIN_SOFTROBOTS=ON -DPLUGIN_SOFAPYTHON3=ON -DSTLIB=ON -S /home/nick/sofa/src -B /home/nick/sofa/$version
+cmake -G "CodeBlocks - Ninja" -DCMAKE_C_COMPILER=/usr/bin/clang-12 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-12 -DCMAKE_PREFIX_PATH=/home/nick/Qt/5.13.2/gcc_64 -DSOFA_EXTERNAL_DIRECTORIES=/home/nick/sofa-plugins -Dpybind11_DIR=/home/nick/.local/lib/python3.8/site-packages/pybind11/share/cmake/pybind11 -DPLUGIN_SOFTROBOTS=ON -DPLUGIN_SOFAPYTHON3=ON -DPLUGIN_STLIB=ON -S /home/nick/sofa/src -B /home/nick/sofa/$version
 
 ninja
 
 rm -rf /home/nick/sofa/build
 mkdir /home/nick/sofa/build
 cp -a /home/nick/sofa/$version/. /home/nick/sofa/build
+
+while true; do
+    read -p "Do you wish to run SOFA? [Y/n] " yn
+    case "${yn:-Y}" in
+        [Yy]* ) $SOFA_BLD/bin/runSofa; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
--- setupSofa.sh
+++ setupSofa.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
 cd ~ 
 mkdir ~/sofa
 mkdir ~/sofa/src
 mkdir ~/sofa/build
 
 sudo apt update && sudo apt upgrade -y
-sudo snap install --classic code
 sudo apt install git -y
+
+read -p "Enter your email for git: " email
+git config --global user.email $email
+read -p "Enter your name for git: " name
+git config --global user.name $name
+
 git clone https://github.com/sofa-framework/sofa.git ~/sofa/src
 cd ~/sofa/src && git checkout v21.12
 
@@ -19,16 +26,23 @@ else
     echo "cmake downloader file exists."
 fi
 tar xf cmake-3.22.2-linux-x86_64.tar.gz
-printf "export PATH=\"`pwd`/cmake-3.22.2-linux-x86_64/bin:\$PATH\" # save it in .bashrc if needed\n\n" >> .bashrc
+
+if [ ! command -v code &> /dev/null ]
+then
+    sudo snap install --classic code
+    printf "\nexport PATH=\"`pwd`/cmake-3.22.2-linux-x86_64/bin:\$PATH\" # save it in .bashrc if needed\n\n" >> .bashrc
+    printf "\nexport PYTHONPATH=\"\$HOME/sofa-plugins/STLIB/python3/src:\$HOME/sofa/build/lib/python3/site-packages\"\nexport SP3_BLD=$HOME/sofa/build\nexport SOFA_BLD=\$HOME/sofa/build\nexport SOFA_ROOT=\$HOME/sofa/build\nexport GUROBI_HOME=\"/opt/gurobi952/linux64\"\nexport PATH=\"\${PATH}:\${GUROBI_HOME}/bin\"\nexport LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:\${GUROBI_HOME}/lib\"\nexport GRB_LICENSE_FILE=/opt/gurobi952/gurobi.lic\n\n " >> .bashrc
+    source ~/.bashrc
+    exit
+fi
+
+
 sudo apt install ninja-build ccache libxcb-xinerama0 libopengl0 libboost-all-dev python3 python3-numpy python3-scipy python3-pip -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 sudo apt install libpng-dev libjpeg-dev libtiff-dev libglew-dev zlib1g-dev libeigen3-dev libcgal-dev libcgal-qt5-dev liboce-ocaf-dev libassimp-dev nvidia-cuda-toolkit libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libblas-dev liblapack-dev -y
 
 yes | pip install pybind11==2.6.0
 
-cd ~
-printf "\nexport PYTHONPATH=\"\$HOME/sofa-plugins/STLIB/python3/src:\$HOME/sofa/build/lib/python3/site-packages\"\nexport SP3_BLD=$HOME/sofa/build\nexport SOFA_BLD=\$HOME/sofa/build\nexport SOFA_ROOT=\$HOME/sofa/build\nexport GUROBI_HOME=\"/opt/gurobi952/linux64\"\nexport PATH=\"\${PATH}:\${GUROBI_HOME}/bin\"\nexport LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:\${GUROBI_HOME}/lib\"\nexport GRB_LICENSE_FILE=/opt/gurobi952/gurobi.lic\n\n " >> .bashrc
-source ~/.bashrc
 
 mkdir ~/sofa-plugins
 git clone https://github.com/SofaDefrost/SoftRobots ~/sofa-plugins/SoftRobots
@@ -55,8 +69,42 @@ then
 else
     echo "Qt downloader file exists."
 fi
-chmod +x qt-unified-linux-x64-online.run
-./qt-unified-linux-x64-online.run 
 
-exec bash
+if [ ! -d "$HOME/Qt/" ]
+then
+    chmod +x qt-unified-linux-x64-online.run
+    ./qt-unified-linux-x64-online.run 
+fi
+
 cmake -G "CodeBlocks - Ninja" -DCMAKE_C_COMPILER=/usr/bin/clang-12 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-12 -DCMAKE_PREFIX_PATH=/home/nick/Qt/5.13.2/gcc_64 -S /home/nick/sofa/src -B /home/nick/sofa/build
+cd /home/nick/sofa/build
+ninja
+
+while true; do
+    read -p "Do you wish to run SOFA? [Y/n] " Yn
+    case "${yn:-Y}" in
+        [Yy]* ) $SOFA_BLD/bin/runSofa; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
+if [ ! -f "gcm-linux_amd64.2.0.785.deb" ]
+then
+    wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.785/gcm-linux_amd64.2.0.785.deb
+else
+    echo "git-credential-manager downloader file exists."
+fi
+
+sudo dpkg -i gcm-linux_amd64.2.0.785.deb
+git-credential-manager-core configure
+git config --global credential.credentialStore cache
+git config --global credential.cacheoptions "--timeout 72000"
+
+git config --global credential.guiPrompt false
+git config --global credential.gitHubAuthModes browser
+
+
+git clone https://github.com/cnick1/softRoboticsMR.git
