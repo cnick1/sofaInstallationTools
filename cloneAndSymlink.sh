de5b166effa2e0dc24f8c905ae3bf2e7e02c5e23
Nick <cnick1@vt.edu>
2022-09-14 15:33:59 -0700

    Added scripts for kernel and symlink quality of life

---
+++ cloneAndSymlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /mnt/e/share
+git clone https://github.com/cnick1/sofaInstallationTools.git
+ln -s /mnt/e/share/sofaInstallationTools /home/nick/sofaInstallationTools
+
+git clone https://github.com/cnick1/softRoboticsMR.git
+ln -s /mnt/e/share/softRoboticsMR /home/nick/softRoboticsMR
---
+++ revertKernel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:cappelikan/ppa
+sudo apt update
+sudo apt install mainline
