1b3e8059056616e61548273944356afaa80f933c
Nick <cnick1@vt.edu>
2022-09-14 16:03:11 -0700

    Fixed symbolic link

--- cloneAndSymlink.sh
+++ cloneAndSymlink.sh
@@ -2,9 +2,9 @@
 
 cd /mnt/e/share
 git clone https://github.com/cnick1/sofaInstallationTools.git
-ln -s /mnt/e/share/sofaInstallationTools /home/nick/sofaInstallationTools
+ln -s /mnt/e/share/sofaInstallationTools /home/nick
 
 git clone https://github.com/cnick1/softRoboticsMR.git
-ln -s /mnt/e/share/softRoboticsMR /home/nick/softRoboticsMR
+ln -s /mnt/e/share/softRoboticsMR /home/nick
 
 echo "IN ORDER TO USE THE TRASH BIN ON THE DRIVE, EDIT THE MOUNT OPTIONS TO INCLUDE: uid=1000,gid=1000"
